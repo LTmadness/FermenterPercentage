74772b32acdfb266a687105bc5173b6766fc5520
LTmadness <ainu.nam0@gmail.com>
2021-03-04 19:17:13 +0000

    added option to have option to use timer instead of percentage

--- FermenterPercentage/FermenterPercentage.cs
+++ FermenterPercentage/FermenterPercentage.cs
@@ -1,21 +1,31 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 using HarmonyLib;
 using System;
+using System.Reflection;
+using UnityEngine;
 
 namespace FermenterPercentage
 {
 
-    [BepInPlugin("org.ltmadness.valheim.fermenterpercentage", "Fermenter Percentage", "0.0.2")]
+    [BepInPlugin(GUID, "Fermenter Percentage", "0.0.3")]
     public class FermenterPercentage : BaseUnityPlugin
     {
+        private const string GUID = "org.ltmadness.valheim.fermenterpercentage";
+
+        private static ConfigEntry<bool> useTime;
+
         public void Awake()
         {
-            Harmony.CreateAndPatchAll(typeof(FermenterPercentage), null);
+            useTime = Config.Bind<bool>("General", "Show time left until done", false, "instead of percentage it shows timer in minutes how much left until fermentig finished");
+
+            Harmony.CreateAndPatchAll(typeof(FermenterPercentage), GUID);
         }
+
 
         [HarmonyPatch(typeof(Fermenter), "GetHoverText")]
         [HarmonyPrefix]
-        public static bool GetHoverText_modded(ref Fermenter __instance, ref string __result)
+        public static bool GetHoverText_light(ref Fermenter __instance, ref string __result)
         {
             ZNetView m_nview = (ZNetView)AccessTools.Field(typeof(Fermenter), "m_nview").GetValue(__instance);
             if (!m_nview.GetZDO().GetString("Content", "").IsNullOrWhiteSpace())
@@ -24,11 +34,27 @@ namespace FermenterPercentage
                 if (d.Ticks != 0L)
                 {
                     double time = (ZNet.instance.GetTime() - d).TotalSeconds;
-                    if (time < (double)__instance.m_fermentationDuration)
+                    if (!time.Equals(-1) && time < (double)__instance.m_fermentationDuration)
                     {
                         string contentName = (string)AccessTools.Method(typeof(Fermenter), "GetContentName").Invoke(__instance, new object[] { });
-                        int percentage = (int)(time / (double)__instance.m_fermentationDuration * 100);
-                        __result = Localization.instance.Localize(__instance.m_name + "( " + contentName + ", $piece_fermenter_fermenting " + percentage + "% )");
+                        if ((bool)AccessTools.Field(typeof(Fermenter), "m_exposed").GetValue(__instance))
+                        {
+                            __result =  Localization.instance.Localize(__instance.m_name + " ( " + contentName + ", $piece_fermenter_exposed )");
+                        }
+
+                        if (!useTime.Value)
+                        {
+                            int percentage = (int)(time / (double)__instance.m_fermentationDuration * 100);
+                            __result = Localization.instance.Localize(__instance.m_name + "( " + contentName + ", $piece_fermenter_fermenting " + percentage + "% )");
+                        }
+                        else
+                        {
+                            double left = ((double)__instance.m_fermentationDuration) - time;
+                            int min = (int)Math.Floor(left / 60);
+                            int sec = ((int)left) % 60;
+
+                            __result = Localization.instance.Localize(__instance.m_name + "( " + contentName + ", $piece_fermenter_fermenting " + min + "m " + sec + "s )");
+                        }
                         return false;
                     }
                 }
