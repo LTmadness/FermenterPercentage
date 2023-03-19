d8b66c7790ebc02b9c95c6b8349f0e7a7faa442b
Arturas Kuliavas <ainu.nam0@gmail.com>
2023-03-19 14:59:51 +0000

    new feature, fixes for lated Valheim release

--- FermenterPercentage/FermenterPercentage.cs
+++ FermenterPercentage/FermenterPercentage.cs
@@ -2,37 +2,41 @@
 using BepInEx.Configuration;
 using HarmonyLib;
 using System;
-using UnityEngine;
-using UnityEngine.UI;
+using System.Text.RegularExpressions;
 
 namespace FermenterPercentage
 {
 
-    [BepInPlugin(GUID, "Fermenter Percentage", "1.0.0")]
+    [BepInPlugin(GUID, "Fermenter Percentage", "1.1.0")]
     public class FermenterPercentage : BaseUnityPlugin
     {
         private const string GUID = "org.ltmadness.valheim.fermenterpercentage";
+        private const string colorRegexPatern = "#(([0-9a-fA-F]{2}){3,4}|([0-9a-fA-F]){3,4})";
 
         private static ConfigEntry<ProgressText> progressText;
         private static ConfigEntry<bool> color;
-        private static ConfigEntry<ProgressBar> progressBar;
+        private static ConfigEntry<string> customColor;
 
-        private static HudData hudData;
+        private static Regex colorRegex;
 
         public void Awake()
         {
-            progressText = Config.Bind<ProgressText>("General", "Progress Text", ProgressText.PERCENT, "Should the progress text be off/percent/time left");
-            color = Config.Bind<bool>("Advanced", "Use color", false, "Adds smooth color change to %/time");
-            progressBar = Config.Bind<ProgressBar>("Advanced", "Progress Bar", ProgressBar.OFF, "Should the progress bar be off/red/yellow");
+            progressText = Config.Bind("General", "Progress Text", ProgressText.PERCENT, "Should the progress text be off/percent/time left");
+            color = Config.Bind("Advanced", "Use color", false, "Adds smooth color change to %/time");
+            customColor = Config.Bind("Advanced", "Custom Static Color", "", "If left black uses default, format of color #RRGGBB");
 
             Config.Save();
+
+            System.Console.WriteLine($"Progress Text: {progressText.Value}, Use color: {color.Value}, Custom Static Color: {customColor.Value}");
+
+            colorRegex = new Regex(colorRegexPatern);
 
             Harmony.CreateAndPatchAll(typeof(FermenterPercentage), GUID);
         }
 
         [HarmonyPatch(typeof(Fermenter), "GetHoverText")]
         [HarmonyPostfix]
-        public static void GetHoverText_light(ref Fermenter __instance, ref string __result)
+        public static void GetHoverText(ref Fermenter __instance, ref string __result)
         {
             if (__result.IsNullOrWhiteSpace())
             {
@@ -47,64 +51,14 @@ namespace FermenterPercentage
                 {
                     double timePassed = (ZNet.instance.GetTime() - d).TotalSeconds;
 
-                    if (!timePassed.Equals(-1) && timePassed < (double)__instance.m_fermentationDuration)
+                    if (!timePassed.Equals(-1) && timePassed < __instance.m_fermentationDuration)
                     {
-
-                        if ((bool)AccessTools.Field(typeof(Fermenter), "m_exposed").GetValue(__instance))
-                        {
-                            if (hudData != null)
-                            {
-                                hudData.m_gui.SetActive(false);
-                            }
-                            return;
-                        }
-
-                        float percentage = (float)(timePassed / (double)__instance.m_fermentationDuration * 100);
+                        float percentage = (float)(timePassed / __instance.m_fermentationDuration * 100);
                         int perc = (int)percentage;
-
-                        if (hudData == null && !ProgressBar.OFF.Equals(progressBar.Value))
-                        {
-                            hudData = new HudData();
-
-                            hudData.m_gui = UnityEngine.Object.Instantiate<GameObject>(EnemyHud.instance.m_baseHud, EnemyHud.instance.m_hudRoot.transform);
-                            hudData.m_gui.SetActive(true);
-                            hudData.m_gui.transform.position = Hud.instance.m_crosshair.transform.position;
-                            hudData.m_healthRoot = hudData.m_gui.transform.Find("Health").gameObject;
-                            hudData.m_healthFast = hudData.m_healthRoot.transform.Find("health_fast").GetComponent<GuiBar>();
-                            hudData.m_healthSlow = hudData.m_healthRoot.transform.Find("health_slow").GetComponent<GuiBar>();
-                            hudData.m_name = hudData.m_gui.transform.Find("Name").GetComponent<Text>();
-                            hudData.m_name.text = "";
-                            hudData.m_level2 = hudData.m_gui.transform.Find("level_2") as RectTransform;
-                            hudData.m_level2.gameObject.SetActive(false);
-                            hudData.m_level3 = hudData.m_gui.transform.Find("level_3") as RectTransform;
-                            hudData.m_level3.gameObject.SetActive(false);
-                            hudData.m_alerted = hudData.m_gui.transform.Find("Alerted") as RectTransform;
-                            hudData.m_alerted.gameObject.SetActive(false);
-                            hudData.m_aware = hudData.m_gui.transform.Find("Aware") as RectTransform;
-                            hudData.m_aware.gameObject.SetActive(false);
-                        }
-
-                        if (hudData != null)
-                        {
-                            hudData.m_gui.transform.position = Hud.instance.m_crosshair.transform.position;
-                            hudData.m_gui.SetActive(true);
-                            if (ProgressBar.YELLOW.Equals(progressBar.Value))
-                            {
-                                hudData.m_healthSlow.SetValue(percentage);
-                                hudData.m_healthSlow.SetMaxValue(100);
-                                hudData.m_healthFast.SetValue(0);
-                            }
-                            else
-                            {
-                                hudData.m_healthFast.SetValue(percentage);
-                                hudData.m_healthFast.SetMaxValue(100);
-                                hudData.m_healthSlow.SetValue(0);
-                            }
-                        }
 
                         if (color.Value)
                         {
-                            string colorHex = $"#{(255 / 100 * (100 - perc)):X2}{(255 / 100 * perc):X2}{0:X2}";
+                            string colorHex = GetColor(perc);
 
                             if (ProgressText.PERCENT.Equals(progressText.Value))
                             {
@@ -112,7 +66,7 @@ namespace FermenterPercentage
                             }
                             else if (ProgressText.TIME.Equals(progressText.Value))
                             {
-                                __result = __result.Replace(")", getTimeResult(__instance, timePassed, colorHex));
+                                __result = __result.Replace(")", GetTimeResult(__instance, timePassed, colorHex));
                             }
                             return;
                         }
@@ -124,39 +78,16 @@ namespace FermenterPercentage
                             }
                             else if (ProgressText.TIME.Equals(progressText.Value))
                             {
-                                __result = __result.Replace(")", getTimeResult(__instance, timePassed, null));
-
+                                __result = __result.Replace(")", GetTimeResult(__instance, timePassed, null));
                             }
                             return;
                         }
-
                     }
                 }
             }
-            if (hudData != null)
-            {
-                hudData.m_gui.SetActive(false);
-            }
         }
 
-        [HarmonyPatch(typeof(Hud), "UpdateCrosshair")]
-        [HarmonyPrefix]
-        public static void UpdateCrosshair(Player player, float bowDrawPercentage)
-        {
-            GameObject hoverObject = player.GetHoverObject();
-            object hoverable = (bool)(UnityEngine.Object)hoverObject ? hoverObject.GetComponentInParent<Hoverable>() : null;
-
-            if (!(hoverable is Fermenter))
-            {
-                if (hudData != null)
-                {
-                    hudData.m_gui.SetActive(false);
-                }
-            }
-
-        }
-
-        public static string getTimeResult(Fermenter __instance, double timePassed, string colorHex)
+        public static string GetTimeResult(Fermenter __instance, double timePassed, string colorHex)
         {
             double left = ((double)__instance.m_fermentationDuration) - timePassed;
             int min = (int)Math.Floor(left / 60);
@@ -170,20 +101,15 @@ namespace FermenterPercentage
             return String.Format($"{min}m {sec}s )");
         }
 
-        private class HudData
+        public static string GetColor(int percentage)
         {
-            //public Character m_character;
-            //public BaseAI m_ai;
-            public GameObject m_gui;
-            public GameObject m_healthRoot;
-            public RectTransform m_level2;
-            public RectTransform m_level3;
-            public RectTransform m_alerted;
-            public RectTransform m_aware;
-            public GuiBar m_healthFast;
-            public GuiBar m_healthSlow;
-            public Text m_name;
-            public float m_hoverTimer = 0f;
+            var csc = customColor.Value.Trim();
+            if (!csc.IsNullOrWhiteSpace() && colorRegex.IsMatch(csc))
+            {
+                return customColor.Value;
+            }
+
+            return $"#{255 / 100 * (100 - percentage):X2}{255 / 100 * percentage:X2}{0:X2}";
         }
 
         enum ProgressText
@@ -191,13 +117,6 @@ namespace FermenterPercentage
             OFF,
             PERCENT,
             TIME
-        }
-
-        enum ProgressBar
-        {
-            OFF,
-            RED,
-            YELLOW
         }
     }
 }
--- README.md
+++ README.md
@@ -7,22 +7,24 @@ Small mod only add percentage of how much have already fermented.
 If you would like to support me you can do it here: https://streamlabs.com/ltmadness/tip
 
 ### Changes
+v1.1.0
+- Fix issues of Fermenter thinks it's a Greydwarf! (Thanks Karmacopper)
+- Removed progress bar (Sorry for those that used it)
+- Can used static custom color (format #RRGGBB)
+- Dependency update
+
+v1.0.1
+- Dependency update
+- Code clean-up
+
 v1.0.0
 - Fixed countdown when someone uses color and time options at once
 - Updated to support BepInEx-5.4.1900 and Valheim Patch 0.208.1
 
 v0.0.6
-- Updated with new BepInEx and TEsted with Valheim 0.202.19 update
+- Updated with new BepInEx and Tested with Valheim 0.202.19 update
 
 v0.0.5
 - New Progress Bar can be enabled using config(Red/Yellow)
 - Rework for config settings to be cleaner
 
-v0.0.4
-- New config option to enable color on % done/time left
-
-v0.0.3
-- Add ability to change to time left until fermentation finished instead of percantage of fermentation done
-
-v0.0.2
-- Update to be cleaner and more afficiant
